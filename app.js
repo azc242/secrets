7a6dbbd5de1389bb38d372b139133f0eae12b1fd
Alan Chen <azc242@nyu.edu>
2020-06-25 17:12:21 -0700

    implement hashing and remove encryption

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -26,7 +27,7 @@ const userSchema = new mongoose.Schema({
 
 const secret = "thisIsMySecret";
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,7 +42,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save();
@@ -57,7 +58,9 @@ app.post("/login", function(req, res) {
 
   User.findOne({email: req.body.username}, function(err, foundUser) {
     if(!err){
-      if(foundUser.password === req.body.password){
+
+      const password = md5(req.body.password);
+      if(foundUser.password === password){
         res.render("secrets");
       }
     }
